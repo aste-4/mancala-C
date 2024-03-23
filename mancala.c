23f01c6cc2984ad6ac8daf7dc6f48ffac98d4d01
aste <34529552+aste-4@users.noreply.github.com>
2024-03-23 17:03:59 +0000

    clang-formatのルールファイル追加および適用

--- mancala.c
+++ mancala.c
@@ -1,13 +1,13 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 #define BUFSIZE 256
 
 typedef struct board {
-    int player;  //0or1
-    int pitID;   //store:-1
+    int player;  // 0or1
+    int pitID;   // store:-1
     int stone;
     struct board *next;
 } BOARD, *PBOARD;
@@ -20,7 +20,7 @@ int sum_player_stones(int player);
 void print_board();
 
 int main(int argc, char const *argv[]) {
-    int player= 0, pitID;
+    int player = 0, pitID;
     init_board(3, 3);
     while(sum_player_stones(0) * sum_player_stones(1) != 0) {
         do {
@@ -30,9 +30,9 @@ int main(int argc, char const *argv[]) {
             while(getchar() != '\n') {
             };
         } while(sowing(player, pitID) && sum_player_stones(0) * sum_player_stones(1) != 0);
-        player= 1 - player;
+        player = 1 - player;
     }
-    player= 1 - player;
+    player = 1 - player;
     print_board();
     printf("The winner is player %d!\n", player);
     return 0;
@@ -41,40 +41,40 @@ int main(int argc, char const *argv[]) {
 void init_board(int pit_num, int stone_num) {
     if(pit_num <= 0 || stone_num <= 0) exit(EXIT_FAILURE);
     free(Binfo);
-    PBOARD pit= malloc(sizeof(BOARD));
-    PBOARD ptr= pit, prev= NULL;
-    int i= 0;
-    for(i= 0; i < (pit_num + 1) * 2; i++) {
-        ptr->player= i / (pit_num + 1);
-        ptr->pitID= (i % (pit_num + 1) == pit_num) ? -1 : i % (pit_num + 1);
-        ptr->stone= (ptr->pitID < 0) ? 0 : stone_num;
-        ptr->next= malloc(sizeof(BOARD));
-        prev= ptr;
-        ptr= ptr->next;
+    PBOARD pit = malloc(sizeof(BOARD));
+    PBOARD ptr = pit, prev = NULL;
+    int i = 0;
+    for(i = 0; i < (pit_num + 1) * 2; i++) {
+        ptr->player = i / (pit_num + 1);
+        ptr->pitID = (i % (pit_num + 1) == pit_num) ? -1 : i % (pit_num + 1);
+        ptr->stone = (ptr->pitID < 0) ? 0 : stone_num;
+        ptr->next = malloc(sizeof(BOARD));
+        prev = ptr;
+        ptr = ptr->next;
     }
     free(ptr);
-    prev->next= pit;
-    Binfo= pit;
+    prev->next = pit;
+    Binfo = pit;
 }
 
 bool sowing(int player, int pitID) {
-    int pick= 0;
-    PBOARD ptr= Binfo;
+    int pick = 0;
+    PBOARD ptr = Binfo;
     do {
         if(ptr->player == player && ptr->pitID == pitID) {
-            pick= ptr->stone;
+            pick = ptr->stone;
             break;
         }
-        ptr= ptr->next;
+        ptr = ptr->next;
     } while(ptr != Binfo);
 
     if(pick == 0 || pitID < 0) {
         printf("Sowing failed. Please try again.\n");
         return true;
     }
-    ptr->stone= 0;
+    ptr->stone = 0;
     while(pick > 0) {
-        ptr= ptr->next;
+        ptr = ptr->next;
         ptr->stone++;
         pick--;
     }
@@ -86,20 +86,20 @@ bool sowing(int player, int pitID) {
 }
 
 int sum_player_stones(int player) {
-    PBOARD ptr= Binfo;
-    int sum= 0;
+    PBOARD ptr = Binfo;
+    int sum = 0;
     do {
         if(ptr->player == player && ptr->pitID >= 0) {
-            sum+= ptr->stone;
+            sum += ptr->stone;
         }
-        ptr= ptr->next;
+        ptr = ptr->next;
     } while(ptr != Binfo);
     return sum;
 }
 
 void print_board() {
-    PBOARD ptr= Binfo;
-    char p0info[BUFSIZE]= "", p1info[BUFSIZE]= "", buf[BUFSIZE];
+    PBOARD ptr = Binfo;
+    char p0info[BUFSIZE] = "", p1info[BUFSIZE] = "", buf[BUFSIZE];
     if(ptr == NULL) {
         printf("The board is not initialized.\n");
         return;
@@ -112,7 +112,7 @@ void print_board() {
             snprintf(buf, BUFSIZE, "[%2d] %2d %s", ptr->pitID, ptr->stone, p1info);
             snprintf(p1info, BUFSIZE, "%s", buf);
         }
-        ptr= ptr->next;
+        ptr = ptr->next;
     } while(ptr != Binfo);
     printf("{player:1} %s        (stones in pit: %2d)\n", p1info, sum_player_stones(1));
     printf("{player:0}         %s(stones in pit: %2d)\n", p0info, sum_player_stones(0));
